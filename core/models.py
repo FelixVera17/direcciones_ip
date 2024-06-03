8994b67804f008105e4fc8644af9adf1f7edcb66
Felix Vera <verafelixbaetcke03@gmail.com>
2024-06-03 08:54:20 -0400

    agregando modelos de las bases de datos y el gitignore.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True, max_length=1000)
+    username = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
+
+class MacAddress(models.Model):
+    id = models.AutoField(primary_key=True, max_length=10000) 
+    ip_address = models.CharField(max_length=250)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE,max_length=10000)
+    
+
